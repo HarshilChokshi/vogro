375e14a14bb76a482d1305db2cd46689dfdd66b2
shusilshapkota <shusilshapkota@gmail.com>
2020-05-01 03:48:59 -0400

    User logouts with GET request

--- vogro_django/vogro_django/settings.py
+++ vogro_django/vogro_django/settings.py
@@ -153,6 +153,7 @@ ACCOUNT_AUTHENTICATION_METHOD = "username_email"
 ACCOUNT_CONFIRM_EMAIL_ON_GET = True
 ACCOUNT_EMAIL_CONFIRMATION_ANONYMOUS_REDIRECT_URL = reverse_lazy('account_confirm_complete')
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL = reverse_lazy('account_confirm_complete')
+ACCOUNT_LOGOUT_ON_GET = True
 
 REST_AUTH_SERIALIZERS = {
     'TOKEN_SERIALIZER': 'auth_api.serializers.TokenSerializer',
