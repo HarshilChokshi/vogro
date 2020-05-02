4fee3b94af918a23d76cf9b4bf5257a0aeeb44ba
Harshil Chokshi <Harsh@Harshils-MacBook-Pro.local>
2020-05-02 19:29:52 -0400

    Added ClientGroceryStore endpoints, and changed model properties for some models.

--- vogro_django/vogro_api/urls.py
+++ vogro_django/vogro_api/urls.py
@@ -2,20 +2,30 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Volunteer user endpoints
     path('volunteeruser/<int:user_id>', views.volunteerUser),
     path('volunteeruser/add_user', views.addVolunteerUser),
     path('volunteeruser/match_lat_long', views.launchAllVolunteerUsersMatchingLocationJob),
+    # LiveGroceryPost endpoints
     path('livegrocerypost/create_post', views.createLiveGroceryPost),
     path('livegrocerypost/volunteeruser/<int:user_id>', views.getAllLiveGroceryPostsByVolunteer),
     path('livegrocerypost/clientuser/<int:user_id>', views.getAllLiveGroceryPostsByClient),
     path('livegrocerypost/attach_receipt/<int:livegrocerypost_id>', views.addReceiptToLiveGroceryPost),
     path('livegrocerypost/<int:livegrocerypost_id>', views.deleteLiveGroceryPost),
+    # Client user endpoints
     path('clientuser/<int:user_id>', views.clientUser),
     path('clientuser/add_user', views.addClientUser),
+    # Complaints endpoints
     path('complaints/create_complaint', views.createComplaint),
     path('complaints/volunteer/<int:user_id>', views.getComplaintsByVolunteer),
     path('complaints/client/<int:user_id>', views.getComplaintsByClient),
+    # CompletedGroceryPost endpoints
     path('completedgrocerypost/create_post', views.createCompletedGroceryPost),
     path('completedgrocerypost/volunteeruser/<int:user_id>', views.getAllCompletedGroceryPostsByVolunteer),
     path('completedgrocerypost/clientuser/<int:user_id>', views.getAllCompletedGroceryPostsByVolunteer),
+    # MatchedGroceryStore endpoints
+    path('clientgrocerypost/create_post', views.createClientGroceryPost),
+    path('clientgrocerypost/clientuser/<int:client_id>', views.getAllClientGroceryPostsBelongingToClient),
+    path('clientgrocerypost/<int:grocery_post_id>', views.clientGroceryPost),
+    path('clientgrocerypost/get_nearby_posts', views.getAllClientGroceryPostNearArea),
 ]
--- vogro_django/vogro_api/views.py
+++ vogro_django/vogro_api/views.py
@@ -4,7 +4,9 @@ from .constants import *
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+import operator
 from datetime import datetime
+from django.db.models import Q
 # Create your views here.
 
 
@@ -79,7 +81,7 @@ def launchAllVolunteerUsersMatchingLocationJob(request):
     for volunteerUser in allVolunteerUsers:
         grocery_stores_preferred = json.loads(volunteerUser.preferred_grocery_stores)
         for store in grocery_stores_preferred:
-            if get_kilometer_distance_between_two_points(lat, long, store['lat'], store['long']) < 100:
+            if getMeterDistanceBetweenTwoLocations(lat, long, store['lat'], store['long']) < 100:
                 targetVolunteerIds.append(volunteerUser.id)
 
     return JsonResponse({'target_volunteer_user_ids': targetVolunteerIds})
@@ -100,8 +102,6 @@ def createLiveGroceryPost(request):
     grocery_item_list_string = json.dumps(body_dict['grocery_item_list'])
 
     # Parse out date and time objects from strings in json
-    time_of_grocery_shopping = datetime.strptime(body_dict['time_of_grocery_shopping'], dateFormatString)
-    time_of_post = datetime.strptime(body_dict['time_of_post'], dateFormatString)
     earliest_time = datetime.strptime(body_dict['earliest_time'], dateFormatString)
     latest_time = datetime.strptime(body_dict['latest_time'], dateFormatString)
 
@@ -112,11 +112,9 @@ def createLiveGroceryPost(request):
         grocery_store_address = grocery_address_string,
         grocery_store_address_name = body_dict['grocery_store_address_name'],
         grocery_store_name = body_dict['grocery_store_name'],
-        time_of_grocery_shopping = time_of_grocery_shopping,
         grocery_item_list = grocery_item_list_string,
         earliest_time = earliest_time,
         latest_time = latest_time,
-        time_of_post = time_of_post,
         receipt_image_ref = body_dict['receipt_image_ref'],
         grocery_total_amount = body_dict['grocery_total_amount'],
         state_of_volunteer = body_dict['state_of_volunteer'],
@@ -198,7 +196,7 @@ def clientUser(request, user_id):
     if request.content_type != 'application/json':
         return HttpResponse('The content-type must be application/json.', status=415)
 
-    # grab the volunteer user from db
+    # grab the client user from db
     clientUser = None
     try:
         clientUser = ClientUser.objects.get(id=user_id)
@@ -257,8 +255,6 @@ def createCompletedGroceryPost(request):
     grocery_item_list_string = json.dumps(body_dict['grocery_item_list'])
 
     # Parse out date and time objects from strings in json
-    time_of_grocery_shopping = datetime.strptime(body_dict['time_of_grocery_shopping'], dateFormatString)
-    time_of_post = datetime.strptime(body_dict['time_of_post'], dateFormatString)
     earliest_time = datetime.strptime(body_dict['earliest_time'], dateFormatString)
     latest_time = datetime.strptime(body_dict['latest_time'], dateFormatString)
 
@@ -269,11 +265,9 @@ def createCompletedGroceryPost(request):
         grocery_store_address = grocery_address_string,
         grocery_store_address_name = body_dict['grocery_store_address_name'],
         grocery_store_name = body_dict['grocery_store_name'],
-        time_of_grocery_shopping = time_of_grocery_shopping,
         grocery_item_list = grocery_item_list_string,
         earliest_time = earliest_time,
         latest_time = latest_time,
-        time_of_post = time_of_post,
         receipt_image_ref = body_dict['receipt_image_ref'],
         grocery_total_amount = body_dict['grocery_total_amount'],
     )
@@ -349,3 +343,171 @@ def getComplaintsByClient(request, user_id):
 
     complaintsJsonDict = getAllComplaints(False, user_id)
     return JsonResponse(complaintsJsonDict)
+
+
+@csrf_exempt
+def createClientGroceryPost(request):
+    # Make sure request is POST method and content type is application/json
+    if request.method != 'POST':
+        return HttpResponse('Only the POST verb can be used on this endpoint.', status=405)
+    if request.content_type != 'application/json':
+        return HttpResponse('The content-type must be application/json.', status=415)
+
+    # get the request body and convert it to python dict object
+    body_dict = json.loads(request.body)
+
+    # Serialize grocery store address and grocery store item list
+    grocery_address_string = json.dumps(body_dict['grocery_store_address'])
+    grocery_item_list_string = json.dumps(body_dict['grocery_item_list'])
+
+    # Parse out date and time objects from strings in json
+    time_of_post = datetime.strptime(body_dict['time_of_post'], dateFormatString)
+    earliest_time = datetime.strptime(body_dict['earliest_time'], dateFormatString)
+    latest_time = datetime.strptime(body_dict['latest_time'], dateFormatString)
+
+    # Create the CompletedGroceryPost object and save to database
+    clientGrocerypPost = ClientGroceryPost(
+        client_user_id_id = body_dict['client_user_id'],
+        grocery_store_address = grocery_address_string,
+        grocery_store_address_name = body_dict['grocery_store_address_name'],
+        grocery_store_name = body_dict['grocery_store_name'],
+        grocery_item_list = grocery_item_list_string,
+        earliest_time = earliest_time,
+        latest_time = latest_time,
+        time_of_post = time_of_post,
+    )
+    clientGrocerypPost.save()
+    return HttpResponse('Successfully added CientGroceryPost object', status=200)
+
+
+
+@csrf_exempt
+def getAllClientGroceryPostsBelongingToClient(request, client_id):
+    # Make sure request is GET method and content type is application/json
+    if request.method != 'GET':
+        return HttpResponse('Only the GET verb can be used on this endpoint.', status=405)
+    if request.content_type != 'application/json':
+        return HttpResponse('The content-type must be application/json.', status=415)
+
+    # grab all client grocery posts from database
+    clientGroceryPostList = ClientGroceryPost.objects.filter(client_user_id=client_id)
+
+    # Convert it to json
+    clientGroceryPostJsonList = []
+    for post in clientGroceryPostList:
+        clientGroceryPostJsonList.append(ClientGroceryPost.convertToJsonDict(post))
+
+    # return json object
+    return JsonResponse({"result_list": clientGroceryPostJsonList})
+
+
+@csrf_exempt
+def clientGroceryPost(request, grocery_post_id):
+    # Make sure content type is application/json
+    if request.content_type != 'application/json':
+        return HttpResponse('The content-type must be application/json.', status=415)
+
+    # grab the ClientGroceryPost from db
+    clientGroceryPost = None
+    try:
+        clientGroceryPost = ClientGroceryPost.objects.get(id=grocery_post_id)
+    except ClientGroceryPost.DoesNotExist:
+        return HttpResponse(f'ClientGroceryPost with id: {grocery_post_id}, does not exist', status=404)
+
+    if request.method == 'PATCH':
+        # Get the request body
+        body_dict = json.loads(request.body)
+        fields_to_change_list = body_dict['fields_to_change']
+
+        for field_name, field_new_value in fields_to_change_list.items():
+            try:
+                getattr(clientGroceryPost, field_name)
+            except AttributeError:
+                continue
+            if field_name == 'grocery_store_address' or field_name == 'grocery_item_list':
+                field_new_value = json.dumps(field_new_value)
+            elif field_name == 'time_of_post' or \
+                    field_name == 'earliest_time' or \
+                    field_name == 'latest_time':
+                    field_new_value = datetime.strptime(field_new_value, dateFormatString)
+            setattr(clientGroceryPost, field_name, field_new_value)
+        # Save the object to the database
+        clientGroceryPost.save()
+        return HttpResponse('Successfully updated ClientGraoceryPost object', status=200)
+    elif request.method == 'POST':
+        return HttpResponse(f'Successfully deleted ClientGroceryPostObject with id {grocery_post_id}', status=200)
+    elif request.method == 'DELETE':
+        clientGroceryPost.delete()
+        return HttpResponse(f'Successfully deleted ClientGroceryPostObject with id {grocery_post_id}', status=200)
+    else:
+        return HttpResponse('Only the PATCH, DELETE, or POST verb can be used on this endpoint.', status=405)
+
+
+
+@csrf_exempt
+def getAllClientGroceryPostNearArea(request):
+    # Make sure request is GET method and content type is application/json
+    if request.method != 'GET':
+        return HttpResponse('Only the GET verb can be used on this endpoint.', status=405)
+    if request.content_type != 'application/json':
+        return HttpResponse('The content-type must be application/json.', status=415)
+
+    # get the request body and convert it to python dict object
+    body_dict = json.loads(request.body)
+
+    # Parse out the time from the body_dict
+    volunteerUserEarliestTime = datetime.strptime(body_dict['earliest_time'], dateFormatString)
+    volunteerUserLatestTime = datetime.strptime(body_dict['latest_time'], dateFormatString)
+
+    # Parse out lat, longd target radius from dict object
+    volunteer_lat = body_dict['lat']
+    volunteer_long = body_dict['long']
+    radius = body_dict['radius']
+
+    # Parse out the field by which user wants the results to be ordered in
+    order_field = body_dict['order_field']
+
+    # Set the time filters for query
+    earliestTimeFilter = Q(earliest_time__range=[volunteerUserEarliestTime, volunteerUserLatestTime])
+    latestTimeFilter = Q(latest_time__range=[volunteerUserEarliestTime, volunteerUserLatestTime])
+
+    clientGorceryPostsResultSet = []
+
+    # Run the query
+    if order_field == 'time_of_post':
+        clientGorceryPostsResultSet = ClientGroceryPost.objects.filter(earliestTimeFilter | latestTimeFilter).order_by(order_field)
+    else:
+        clientGorceryPostsResultSet = ClientGroceryPost.objects.filter(earliestTimeFilter | latestTimeFilter)
+
+    # Filter by specified radius
+    clientGorceryPostsRadiusFilterResultTupleSet = []
+
+    for post in clientGorceryPostsResultSet:
+        # Parse out the grocery store lat and long
+        grocery_store_address_dict = json.loads(post.grocery_store_address)
+        store_lat = grocery_store_address_dict['lat']
+        store_long = grocery_store_address_dict['long']
+
+        # Get the distance
+        distance = getMeterDistanceBetweenTwoLocations(volunteer_lat, volunteer_long, store_lat, store_long)
+
+        # Filter by radius
+        if  distance <= radius:
+            postTuple = (post, distance)
+            clientGorceryPostsRadiusFilterResultTupleSet.append(postTuple)
+
+
+    # Sort results by distance if needed
+    if order_field == 'radius':
+        clientGorceryPostsRadiusFilterResultTupleSet.sort(key=operator.itemgetter(1))
+
+    # convert all objects to json and add distance with each object
+    clientGroceryPostJsonList = []
+    for post in clientGorceryPostsRadiusFilterResultTupleSet:
+        post_dict = {}
+        post_dict['post'] = ClientGroceryPost.convertToJsonDict(post[0])
+        post_dict['distance'] = post[1]
+        clientGroceryPostJsonList.append(post_dict)
+
+    # return json object
+    return JsonResponse({"result_list": clientGroceryPostJsonList})
